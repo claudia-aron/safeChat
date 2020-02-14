2e1de3c7cb999307be75641b0247d404ab47d951
Aron Tolentino <arontolentino@outlook.com>
2020-02-14 16:40:19 -0500

    Create pseudo code for script.js

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
 $(document).ready(function() {
 	console.log('ready!');
+	///================================///
+	// Event listener for submit button
+	///================================///
+
+	// Store input value to variable called comment
+	// Call classifyComment(comment)
+
+	///================================///
+	// Event listener for generate button
+	///================================///
+
+	// Call getRandomSentence()
+
+	///=========================================================================================///
+	// Function that takes in a random number and makes the right API call (getRandomSentence())
+	///=========================================================================================///
+
+	// Generate a random number from 1 to 2
+
+	// If random number is 1 then call getCompliment()
+	//// Store returned value to a variable called randomSentence
+
+	// If random number is 2 then call get Insulty()
+	//// Store returned value to a variable called randomSentence
+
+	// Replace the input value to the value of randomSentence
+
+	///========================================================================///
+	// Function for making an API request to Complementr API (getCompliment())
+	///========================================================================///
+
+	// AJAX request to https://complimentr.com/api
+	// Wait for response
+	// Return response
+
+	///===============================================================///
+	// Function for making an API request to Insult API (getInsult())
+	///===============================================================///
+
+	// AJAX request to https://evilinsult.com/generate_insult.php?lang=en&type=json
+	// Wait for response
+	// Return response
+
+	///========================================================================///
+	// Function for making an API request to Tensorflow.js (classifyComment())
+	///========================================================================///
+
+	// Set prediction threshold
+	// Load tensorflow model
+	// Pass on parameter to model
+	// Wait for response
+
+	// Loop through response
+
+	//// if probabilty is higher than 40
+	////// Notify user of type of toxcity
+	////// Notify user to try again
+
+	//// if probabilty is lower than 40
+	////// Add comment to the comment board
 });

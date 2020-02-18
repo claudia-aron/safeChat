0b38462a8e6fdd285d8e621f110bd713664e21ac
Aron Tolentino <arontolentino@outlook.com>
2020-02-18 08:41:54 -0500

    Style CSS and lay out initial event listeners + functions

--- script.js
+++ script.js
@@ -4,60 +4,66 @@ $(document).ready(function() {
 	// Event listener for submit button
 	///================================///
 
-	// Store input value to variable called comment
-	// Call classifyComment(comment)
+	$('.send').on('click', function() {
+		// Store input value to variable called comment
+		// Call classifyComment(comment)
+	});
 
 	///================================///
 	// Event listener for generate button
 	///================================///
 
-	// Call getRandomSentence()
+	$('.send').on('click', function() {
+		// Call getRandomSentence()
+	});
 
 	///=========================================================================================///
 	// Function that takes in a random number and makes the right API call (getRandomSentence())
 	///=========================================================================================///
 
-	// Generate a random number from 1 to 2
-
-	// If random number is 1 then call getCompliment()
-	//// Store returned value to a variable called randomSentence
-
-	// If random number is 2 then call get Insulty()
-	//// Store returned value to a variable called randomSentence
-
-	// Replace the input value to the value of randomSentence
+	function getRandomSentence() {
+		// Generate a random number from 1 to 2
+		// If random number is 1 then call getCompliment()
+		//// Store returned value to a variable called randomSentence
+		// If random number is 2 then call get Insulty()
+		//// Store returned value to a variable called randomSentence
+		// Replace the input value to the value of randomSentence
+	}
 
 	///========================================================================///
 	// Function for making an API request to Complementr API (getCompliment())
 	///========================================================================///
 
-	// AJAX request to https://complimentr.com/api
-	// Wait for response
-	// Return response
+	function getCompliment() {
+		// AJAX request to https://complimentr.com/api
+		// Wait for response
+		// Return response
+	}
 
 	///===============================================================///
 	// Function for making an API request to Insult API (getInsult())
 	///===============================================================///
 
-	// AJAX request to https://evilinsult.com/generate_insult.php?lang=en&type=json
-	// Wait for response
-	// Return response
+	function getInsult() {
+		// AJAX request to https://evilinsult.com/generate_insult.php?lang=en&type=json
+		// Wait for response
+		// Return response
+	}
 
 	///========================================================================///
 	// Function for making an API request to Tensorflow.js (classifyComment())
 	///========================================================================///
 
-	// Set prediction threshold
-	// Load tensorflow model
-	// Pass on parameter to model
-	// Wait for response
-
-	// Loop through response
-
-	//// if probabilty is higher than 40
-	////// Notify user of type of toxcity
-	////// Notify user to try again
-
-	//// if probabilty is lower than 40
-	////// Add comment to the comment board
+	function classifyComment() {
+		// Set prediction threshold
+		// Load tensorflow model
+		// Pass on parameter to model
+		// Wait for response
+		// Loop through response
+		//// if probabilty is higher than 40
+		////// Notify user of type of toxcity
+		////// Notify user to try again
+		//// if probabilty is lower than 40
+		////// Add comment to the comment board
+	}
 });
